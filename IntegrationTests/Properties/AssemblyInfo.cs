6a457437775b60dbcc9b6545bac14e9a1adb2edb
antonikochet <anton_8598@mail.ru>
2020-02-26 17:54:42 +0300

    add IntegrationTests

---
+++ IntegrationTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("IntegrationTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("IntegrationTests")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("8293a5a6-cffc-4e7d-8160-ed31a33d6ea8")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ IntegrationTests/UnitTest1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using 
+
+namespace IntegrationTests
+{
+    [TestClass]
+    public class IntegrationTest
+    {
+        private List<int> Methods(string text)
+        {
+            return Module2.MainsFor(
+                    Module1.ConvertStringInInt(
+                        Module1.ConvertStringInMasString(text)));
+        }
+        [TestMethod]
+        public void Tests()
+        {
+            string startText = "";
+            CollectionAssert.AreEqual(Methods(startText),
+                new List<int>() { });
+            startText = "153 745 155 741 895 123";
+            CollectionAssert.AreEqual(Methods(startText),
+                new List<int>() {153, 745, 155, 741, 895, 123 });
+            startText = "125 741 841 127 416 511 746 255";
+            CollectionAssert.AreEqual(Methods(startText),
+                new List<int>() { 125, 741, 841, 416, 511, 746, 255 });
+            startText = "125 745 31 7 127 954 35";
+            CollectionAssert.AreEqual(Methods(startText),
+                new List<int>() { 125, 745, 954, 35 });
+
+
+
+        }
+    }
+}
